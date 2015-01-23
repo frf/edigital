38b54ed393d92fe73e8d7c472b56e544b061d819
Fabio Rocha <fabio@fabiofarias.com.br>
2015-01-23 15:37:19 -0200

    Financeiro

---
+++ app/models/propel/Map/MoedaTableMap.php
@@ -0,0 +1,435 @@
+<?php
+
+namespace Map;
+
+use \Moeda;
+use \MoedaQuery;
+use Propel\Runtime\Propel;
+use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\ActiveQuery\InstancePoolTrait;
+use Propel\Runtime\Connection\ConnectionInterface;
+use Propel\Runtime\DataFetcher\DataFetcherInterface;
+use Propel\Runtime\Exception\PropelException;
+use Propel\Runtime\Map\RelationMap;
+use Propel\Runtime\Map\TableMap;
+use Propel\Runtime\Map\TableMapTrait;
+
+
+/**
+ * This class defines the structure of the 'moeda' table.
+ *
+ *
+ *
+ * This map class is used by Propel to do runtime db structure discovery.
+ * For example, the createSelectSql() method checks the type of a given column used in an
+ * ORDER BY clause to know whether it needs to apply SQL to make the ORDER BY case-insensitive
+ * (i.e. if it's a text column type).
+ *
+ */
+class MoedaTableMap extends TableMap
+{
+    use InstancePoolTrait;
+    use TableMapTrait;
+
+    /**
+     * The (dot-path) name of this class
+     */
+    const CLASS_NAME = '.Map.MoedaTableMap';
+
+    /**
+     * The default database name for this class
+     */
+    const DATABASE_NAME = 'edigital';
+
+    /**
+     * The table name for this class
+     */
+    const TABLE_NAME = 'moeda';
+
+    /**
+     * The related Propel class for this table
+     */
+    const OM_CLASS = '\\Moeda';
+
+    /**
+     * A class that can be returned by this tableMap
+     */
+    const CLASS_DEFAULT = 'Moeda';
+
+    /**
+     * The total number of columns
+     */
+    const NUM_COLUMNS = 5;
+
+    /**
+     * The number of lazy-loaded columns
+     */
+    const NUM_LAZY_LOAD_COLUMNS = 0;
+
+    /**
+     * The number of columns to hydrate (NUM_COLUMNS - NUM_LAZY_LOAD_COLUMNS)
+     */
+    const NUM_HYDRATE_COLUMNS = 5;
+
+    /**
+     * the column name for the sigla field
+     */
+    const COL_SIGLA = 'moeda.sigla';
+
+    /**
+     * the column name for the codigo field
+     */
+    const COL_CODIGO = 'moeda.codigo';
+
+    /**
+     * the column name for the simbolo field
+     */
+    const COL_SIMBOLO = 'moeda.simbolo';
+
+    /**
+     * the column name for the nome field
+     */
+    const COL_NOME = 'moeda.nome';
+
+    /**
+     * the column name for the id field
+     */
+    const COL_ID = 'moeda.id';
+
+    /**
+     * The default string format for model objects of the related table
+     */
+    const DEFAULT_STRING_FORMAT = 'YAML';
+
+    /**
+     * holds an array of fieldnames
+     *
+     * first dimension keys are the type constants
+     * e.g. self::$fieldNames[self::TYPE_PHPNAME][0] = 'Id'
+     */
+    protected static $fieldNames = array (
+        self::TYPE_PHPNAME       => array('Sigla', 'Codigo', 'Simbolo', 'Nome', 'Id', ),
+        self::TYPE_CAMELNAME     => array('sigla', 'codigo', 'simbolo', 'nome', 'id', ),
+        self::TYPE_COLNAME       => array(MoedaTableMap::COL_SIGLA, MoedaTableMap::COL_CODIGO, MoedaTableMap::COL_SIMBOLO, MoedaTableMap::COL_NOME, MoedaTableMap::COL_ID, ),
+        self::TYPE_FIELDNAME     => array('sigla', 'codigo', 'simbolo', 'nome', 'id', ),
+        self::TYPE_NUM           => array(0, 1, 2, 3, 4, )
+    );
+
+    /**
+     * holds an array of keys for quick access to the fieldnames array
+     *
+     * first dimension keys are the type constants
+     * e.g. self::$fieldKeys[self::TYPE_PHPNAME]['Id'] = 0
+     */
+    protected static $fieldKeys = array (
+        self::TYPE_PHPNAME       => array('Sigla' => 0, 'Codigo' => 1, 'Simbolo' => 2, 'Nome' => 3, 'Id' => 4, ),
+        self::TYPE_CAMELNAME     => array('sigla' => 0, 'codigo' => 1, 'simbolo' => 2, 'nome' => 3, 'id' => 4, ),
+        self::TYPE_COLNAME       => array(MoedaTableMap::COL_SIGLA => 0, MoedaTableMap::COL_CODIGO => 1, MoedaTableMap::COL_SIMBOLO => 2, MoedaTableMap::COL_NOME => 3, MoedaTableMap::COL_ID => 4, ),
+        self::TYPE_FIELDNAME     => array('sigla' => 0, 'codigo' => 1, 'simbolo' => 2, 'nome' => 3, 'id' => 4, ),
+        self::TYPE_NUM           => array(0, 1, 2, 3, 4, )
+    );
+
+    /**
+     * Initialize the table attributes and columns
+     * Relations are not initialized by this method since they are lazy loaded
+     *
+     * @return void
+     * @throws PropelException
+     */
+    public function initialize()
+    {
+        // attributes
+        $this->setName('moeda');
+        $this->setPhpName('Moeda');
+        $this->setIdentifierQuoting(false);
+        $this->setClassName('\\Moeda');
+        $this->setPackage('');
+        $this->setUseIdGenerator(true);
+        $this->setPrimaryKeyMethodInfo('moeda_id_seq');
+        // columns
+        $this->addColumn('sigla', 'Sigla', 'VARCHAR', false, 10, null);
+        $this->addColumn('codigo', 'Codigo', 'VARCHAR', false, 100, null);
+        $this->addColumn('simbolo', 'Simbolo', 'VARCHAR', false, 100, null);
+        $this->addColumn('nome', 'Nome', 'VARCHAR', false, 100, null);
+        $this->addPrimaryKey('id', 'Id', 'INTEGER', true, null, null);
+    } // initialize()
+
+    /**
+     * Build the RelationMap objects for this table relationships
+     */
+    public function buildRelations()
+    {
+        $this->addRelation('ClientePgtos', '\\ClientePgtos', RelationMap::ONE_TO_MANY, array('id' => 'idmoeda', ), null, null, 'ClientePgtoss');
+        $this->addRelation('Produtos', '\\Produtos', RelationMap::ONE_TO_MANY, array('id' => 'idmoeda', ), null, null, 'Produtoss');
+    } // buildRelations()
+
+    /**
+     * Retrieves a string version of the primary key from the DB resultset row that can be used to uniquely identify a row in this table.
+     *
+     * For tables with a single-column primary key, that simple pkey value will be returned.  For tables with
+     * a multi-column primary key, a serialize()d version of the primary key will be returned.
+     *
+     * @param array  $row       resultset row.
+     * @param int    $offset    The 0-based offset for reading from the resultset row.
+     * @param string $indexType One of the class type constants TableMap::TYPE_PHPNAME, TableMap::TYPE_CAMELNAME
+     *                           TableMap::TYPE_COLNAME, TableMap::TYPE_FIELDNAME, TableMap::TYPE_NUM
+     *
+     * @return string The primary key hash of the row
+     */
+    public static function getPrimaryKeyHashFromRow($row, $offset = 0, $indexType = TableMap::TYPE_NUM)
+    {
+        // If the PK cannot be derived from the row, return NULL.
+        if ($row[TableMap::TYPE_NUM == $indexType ? 4 + $offset : static::translateFieldName('Id', TableMap::TYPE_PHPNAME, $indexType)] === null) {
+            return null;
+        }
+
+        return (string) $row[TableMap::TYPE_NUM == $indexType ? 4 + $offset : static::translateFieldName('Id', TableMap::TYPE_PHPNAME, $indexType)];
+    }
+
+    /**
+     * Retrieves the primary key from the DB resultset row
+     * For tables with a single-column primary key, that simple pkey value will be returned.  For tables with
+     * a multi-column primary key, an array of the primary key columns will be returned.
+     *
+     * @param array  $row       resultset row.
+     * @param int    $offset    The 0-based offset for reading from the resultset row.
+     * @param string $indexType One of the class type constants TableMap::TYPE_PHPNAME, TableMap::TYPE_CAMELNAME
+     *                           TableMap::TYPE_COLNAME, TableMap::TYPE_FIELDNAME, TableMap::TYPE_NUM
+     *
+     * @return mixed The primary key of the row
+     */
+    public static function getPrimaryKeyFromRow($row, $offset = 0, $indexType = TableMap::TYPE_NUM)
+    {
+        return (int) $row[
+            $indexType == TableMap::TYPE_NUM
+                ? 4 + $offset
+                : self::translateFieldName('Id', TableMap::TYPE_PHPNAME, $indexType)
+        ];
+    }
+
+    /**
+     * The class that the tableMap will make instances of.
+     *
+     * If $withPrefix is true, the returned path
+     * uses a dot-path notation which is translated into a path
+     * relative to a location on the PHP include_path.
+     * (e.g. path.to.MyClass -> 'path/to/MyClass.php')
+     *
+     * @param boolean $withPrefix Whether or not to return the path with the class name
+     * @return string path.to.ClassName
+     */
+    public static function getOMClass($withPrefix = true)
+    {
+        return $withPrefix ? MoedaTableMap::CLASS_DEFAULT : MoedaTableMap::OM_CLASS;
+    }
+
+    /**
+     * Populates an object of the default type or an object that inherit from the default.
+     *
+     * @param array  $row       row returned by DataFetcher->fetch().
+     * @param int    $offset    The 0-based offset for reading from the resultset row.
+     * @param string $indexType The index type of $row. Mostly DataFetcher->getIndexType().
+                                 One of the class type constants TableMap::TYPE_PHPNAME, TableMap::TYPE_CAMELNAME
+     *                           TableMap::TYPE_COLNAME, TableMap::TYPE_FIELDNAME, TableMap::TYPE_NUM.
+     *
+     * @throws PropelException Any exceptions caught during processing will be
+     *                         rethrown wrapped into a PropelException.
+     * @return array           (Moeda object, last column rank)
+     */
+    public static function populateObject($row, $offset = 0, $indexType = TableMap::TYPE_NUM)
+    {
+        $key = MoedaTableMap::getPrimaryKeyHashFromRow($row, $offset, $indexType);
+        if (null !== ($obj = MoedaTableMap::getInstanceFromPool($key))) {
+            // We no longer rehydrate the object, since this can cause data loss.
+            // See http://www.propelorm.org/ticket/509
+            // $obj->hydrate($row, $offset, true); // rehydrate
+            $col = $offset + MoedaTableMap::NUM_HYDRATE_COLUMNS;
+        } else {
+            $cls = MoedaTableMap::OM_CLASS;
+            /** @var Moeda $obj */
+            $obj = new $cls();
+            $col = $obj->hydrate($row, $offset, false, $indexType);
+            MoedaTableMap::addInstanceToPool($obj, $key);
+        }
+
+        return array($obj, $col);
+    }
+
+    /**
+     * The returned array will contain objects of the default type or
+     * objects that inherit from the default.
+     *
+     * @param DataFetcherInterface $dataFetcher
+     * @return array
+     * @throws PropelException Any exceptions caught during processing will be
+     *                         rethrown wrapped into a PropelException.
+     */
+    public static function populateObjects(DataFetcherInterface $dataFetcher)
+    {
+        $results = array();
+
+        // set the class once to avoid overhead in the loop
+        $cls = static::getOMClass(false);
+        // populate the object(s)
+        while ($row = $dataFetcher->fetch()) {
+            $key = MoedaTableMap::getPrimaryKeyHashFromRow($row, 0, $dataFetcher->getIndexType());
+            if (null !== ($obj = MoedaTableMap::getInstanceFromPool($key))) {
+                // We no longer rehydrate the object, since this can cause data loss.
+                // See http://www.propelorm.org/ticket/509
+                // $obj->hydrate($row, 0, true); // rehydrate
+                $results[] = $obj;
+            } else {
+                /** @var Moeda $obj */
+                $obj = new $cls();
+                $obj->hydrate($row);
+                $results[] = $obj;
+                MoedaTableMap::addInstanceToPool($obj, $key);
+            } // if key exists
+        }
+
+        return $results;
+    }
+    /**
+     * Add all the columns needed to create a new object.
+     *
+     * Note: any columns that were marked with lazyLoad="true" in the
+     * XML schema will not be added to the select list and only loaded
+     * on demand.
+     *
+     * @param Criteria $criteria object containing the columns to add.
+     * @param string   $alias    optional table alias
+     * @throws PropelException Any exceptions caught during processing will be
+     *                         rethrown wrapped into a PropelException.
+     */
+    public static function addSelectColumns(Criteria $criteria, $alias = null)
+    {
+        if (null === $alias) {
+            $criteria->addSelectColumn(MoedaTableMap::COL_SIGLA);
+            $criteria->addSelectColumn(MoedaTableMap::COL_CODIGO);
+            $criteria->addSelectColumn(MoedaTableMap::COL_SIMBOLO);
+            $criteria->addSelectColumn(MoedaTableMap::COL_NOME);
+            $criteria->addSelectColumn(MoedaTableMap::COL_ID);
+        } else {
+            $criteria->addSelectColumn($alias . '.sigla');
+            $criteria->addSelectColumn($alias . '.codigo');
+            $criteria->addSelectColumn($alias . '.simbolo');
+            $criteria->addSelectColumn($alias . '.nome');
+            $criteria->addSelectColumn($alias . '.id');
+        }
+    }
+
+    /**
+     * Returns the TableMap related to this object.
+     * This method is not needed for general use but a specific application could have a need.
+     * @return TableMap
+     * @throws PropelException Any exceptions caught during processing will be
+     *                         rethrown wrapped into a PropelException.
+     */
+    public static function getTableMap()
+    {
+        return Propel::getServiceContainer()->getDatabaseMap(MoedaTableMap::DATABASE_NAME)->getTable(MoedaTableMap::TABLE_NAME);
+    }
+
+    /**
+     * Add a TableMap instance to the database for this tableMap class.
+     */
+    public static function buildTableMap()
+    {
+        $dbMap = Propel::getServiceContainer()->getDatabaseMap(MoedaTableMap::DATABASE_NAME);
+        if (!$dbMap->hasTable(MoedaTableMap::TABLE_NAME)) {
+            $dbMap->addTableObject(new MoedaTableMap());
+        }
+    }
+
+    /**
+     * Performs a DELETE on the database, given a Moeda or Criteria object OR a primary key value.
+     *
+     * @param mixed               $values Criteria or Moeda object or primary key or array of primary keys
+     *              which is used to create the DELETE statement
+     * @param  ConnectionInterface $con the connection to use
+     * @return int             The number of affected rows (if supported by underlying database driver).  This includes CASCADE-related rows
+     *                         if supported by native driver or if emulated using Propel.
+     * @throws PropelException Any exceptions caught during processing will be
+     *                         rethrown wrapped into a PropelException.
+     */
+     public static function doDelete($values, ConnectionInterface $con = null)
+     {
+        if (null === $con) {
+            $con = Propel::getServiceContainer()->getWriteConnection(MoedaTableMap::DATABASE_NAME);
+        }
+
+        if ($values instanceof Criteria) {
+            // rename for clarity
+            $criteria = $values;
+        } elseif ($values instanceof \Moeda) { // it's a model object
+            // create criteria based on pk values
+            $criteria = $values->buildPkeyCriteria();
+        } else { // it's a primary key, or an array of pks
+            $criteria = new Criteria(MoedaTableMap::DATABASE_NAME);
+            $criteria->add(MoedaTableMap::COL_ID, (array) $values, Criteria::IN);
+        }
+
+        $query = MoedaQuery::create()->mergeWith($criteria);
+
+        if ($values instanceof Criteria) {
+            MoedaTableMap::clearInstancePool();
+        } elseif (!is_object($values)) { // it's a primary key, or an array of pks
+            foreach ((array) $values as $singleval) {
+                MoedaTableMap::removeInstanceFromPool($singleval);
+            }
+        }
+
+        return $query->delete($con);
+    }
+
+    /**
+     * Deletes all rows from the moeda table.
+     *
+     * @param ConnectionInterface $con the connection to use
+     * @return int The number of affected rows (if supported by underlying database driver).
+     */
+    public static function doDeleteAll(ConnectionInterface $con = null)
+    {
+        return MoedaQuery::create()->doDeleteAll($con);
+    }
+
+    /**
+     * Performs an INSERT on the database, given a Moeda or Criteria object.
+     *
+     * @param mixed               $criteria Criteria or Moeda object containing data that is used to create the INSERT statement.
+     * @param ConnectionInterface $con the ConnectionInterface connection to use
+     * @return mixed           The new primary key.
+     * @throws PropelException Any exceptions caught during processing will be
+     *                         rethrown wrapped into a PropelException.
+     */
+    public static function doInsert($criteria, ConnectionInterface $con = null)
+    {
+        if (null === $con) {
+            $con = Propel::getServiceContainer()->getWriteConnection(MoedaTableMap::DATABASE_NAME);
+        }
+
+        if ($criteria instanceof Criteria) {
+            $criteria = clone $criteria; // rename for clarity
+        } else {
+            $criteria = $criteria->buildCriteria(); // build Criteria from Moeda object
+        }
+
+        if ($criteria->containsKey(MoedaTableMap::COL_ID) && $criteria->keyContainsValue(MoedaTableMap::COL_ID) ) {
+            throw new PropelException('Cannot insert a value for auto-increment primary key ('.MoedaTableMap::COL_ID.')');
+        }
+
+
+        // Set the correct dbName
+        $query = MoedaQuery::create()->mergeWith($criteria);
+
+        // use transaction because $criteria could contain info
+        // for more than one table (I guess, conceivably)
+        return $con->transaction(function () use ($con, $query) {
+            return $query->doInsert($con);
+        });
+    }
+
+} // MoedaTableMap
+// This is the static code needed to register the TableMap for this table with the main Propel class.
+//
+MoedaTableMap::buildTableMap();
