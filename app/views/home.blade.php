a84395950c943bf215ec3848b0af3c063a47d5b0
Fabio Rocha <fabio@fabiofarias.com.br>
2015-01-28 23:34:44 -0200

    Ativa modulo

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -6,24 +6,25 @@
     <div class="jumbotron">
         <h2>{{$nomeEmpresa}}</h2>
         @if(Auth::check())
-            <p>Bem vindo {{ $nomeUsuario }}.</p>
+            <p>Olá, {{ $nomeUsuario }}</p>
         @endif
     </div>
-    @if(Auth::check()){
+    
+    @if(Auth::check())
     @if(Auth::user()->tipo == 'cliente')
+    @if(in_array('atendimento',Config::get('edigital.moduloAtivo')))
     <div style="padding-top: 30px; font-size: 13px">
-        <h4><b>Atendimentos</b></h4>
-
+        <h4>Atendimentos</h4>
         <table class="table" id="no-more-tables">
             <thead>
                 <thead>
                     <tr>
-                        <td><b>Id</b></td>
-                        <td><b>Título</b></td>
-                        <td><b>Categoria</b></td>
-                        <td><b>Status</b></td>
-                        <td><b>Data de Cadastro</b></td>
-                        <td><b>Ferramentas</b></td>
+                        <th>Id</th>
+                        <th>Título</th>
+                        <th>Categoria</th>
+                        <th>Status</th>
+                        <th>Data de Cadastro</th>
+                        <th>Ferramentas</th>
                     </tr>
                 </thead>
             </thead>
@@ -36,15 +37,17 @@
                         <td data-title="Status" scope="row">{{ StatusChamado::find($chamado->status)->status_chamado }}</td>
                         <td data-title="Data" scope="row">{{ $chamado->data }}</td>
                         <td data-title="Ferramentas" scope="row">
-                            <a href="/atendimento/historico/{{ $chamado->id }}" class="btn btn-xs btn-primary">Histórico</a>                            
+                            <a href="/atendimento/historico/{{ $chamado->id }}" class="btn btn-xs btn-primary">Visializar</a>                            
                         </td>
                     </tr>
                  @endforeach
             </tbody>
         </table>
     </div>
+    @endif
+    @if(in_array('financeiro',Config::get('edigital.moduloAtivo')))
     <div class="table-responsive table-striped" id="no-more-tables">
-        <h4><b>Financeiro</b></h4>
+        <h4>Financeiro</h4>
       <table class="table">
           <thead>
               <tr>
@@ -72,12 +75,16 @@
                         @else 
                             <a href="#" class="btn btn-xs btn-warning">Pendente</a>
                         @endif
+                        @if($pgto->getNota())
+                            <a href="#" class="btn btn-xs btn-primary">Nota Fiscal</a>
+                        @endif
                   </td>              
                 </tr>
             @endforeach
             </tbody>
       </table>
     </div>
+    @endif
     @endif
     @endif
 @stop
