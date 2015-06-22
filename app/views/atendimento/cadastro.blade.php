24f77184c865ffb3627b314ea255d38115324cb5
Fabio Rocha <fabio@fabiofarias.com.br>
2015-06-22 14:57:51 -0300

    Modulo ativo

--- app/views/atendimento/cadastro.blade.php
+++ app/views/atendimento/cadastro.blade.php
@@ -19,73 +19,52 @@
         <br />
 
         <form method="post" id="cad_chamado">
-            <table>
-                <tr>
-                    <td width="100">
-                        <label for="cat">*Categoria: </label>
-                    </td>
-                    <td width="200">
-                        <select class="form-control" id="cat" name="categoria">
-                            <option value="">Selecione...</option>
-                            @foreach($cat_cha as $aCat_cha)
-                                <option value="{{ $aCat_cha->id }}">{{ $aCat_cha->cat_chamado }}</option>
-                            @endforeach
-                        </select>
-                        <div id="categoria_cadastro" style="position: absolute; display: none">
-                            <i style="color: red; font-size: 10px">Selecione uma categoria.</i>
-                        </div>
-                    </td>
-                    <td width="100" style="padding-left: 10px">
-                        <label for="status">*Status: </label>
-                    </td>
-                    <td width="200">
-                        <select class="form-control" id="status" name="status">
-                            <option value="1">Aberto</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr style="height: 76px">
-                    <td><label>*Título: </label></td>
-                    <td colspan="3">
-                        <input class="form-control" type="text" id="titulo" name="titulo" />
-                        <div id="titulo_cadastro" style="position: absolute; display: none">
-                            <i style="color: red; font-size: 10px">Dê um título para o chamado.</i>
-                        </div>
-                    </td>
-                    <td></td>
-                    <td></td>
-                </tr>
 
-                @if($produto->count()){
-                <tr style="height: 76px">
-                    <td><label>*Produto: </label></td>
-                    <td colspan="3">
-                        <select class="form-control" id="cat" name="categoria">
-                            <option value="">Selecione...</option>
-                            @foreach($produto as $oProd)
-                                <option value="{{ $oProd->id }}">{{ $oProd->nome }}</option>
-                            @endforeach
-                        </select>
-                    </td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                @endif
-                <tr>
-                    <td><label>*Descrição: </label></td>
-                    <td colspan="3">
-                        <textarea style="height: 200px" class="form-control" id="descricao" name="mensagem"></textarea>
-                        <div id="descricao_cadastro" style="position: absolute; display: none">
-                            <i style="color: red; font-size: 10px">Descreva o motivo do chamado.</i>
-                        </div>
-                    </td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </table>
+            <div class="form-group">
+                <label for="cat">*Categoria: </label>
+                <select class="form-control" id="cat" name="categoria">
+                    <option value="">Selecione...</option>
+                    @foreach($cat_cha as $aCat_cha)
+                        <option value="{{ $aCat_cha->id }}">{{ $aCat_cha->cat_chamado }}</option>
+                    @endforeach
+                </select>
+                <div id="categoria_cadastro" style="position: absolute; display: none">
+                    <i style="color: red; font-size: 10px">Selecione uma categoria.</i>
+                </div>
+            </div>
 
-            <br />
-            <br />
+            <div class="form-group">
+                <label for="status">*Status: </label>
+                <select class="form-control" id="status" name="status">
+                    <option value="1">Aberto</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>*Título: </label>
+                <input class="form-control" type="text" id="titulo" name="titulo" />
+                <div id="titulo_cadastro" style="position: absolute; display: none">
+                    <i style="color: red; font-size: 10px">Dê um título para o chamado.</i>
+                </div>
+            </div>
+            @if($produto->count())
+            <div class="form-group">
+                <label>*Produto: </label>
+                <select class="form-control" id="cat" name="categoria">
+                    <option value="">Selecione...</option>
+                    @foreach($produto as $oProd)
+                        <option value="{{ $oProd->id }}">{{ $oProd->nome }}</option>
+                    @endforeach
+                </select>
+            </div>
+            @endif
+
+            <div class="form-group">
+                <label>*Descrição: </label>
+                <textarea style="height: 200px" class="form-control" id="descricao" name="mensagem"></textarea>
+                <div id="descricao_cadastro" style="position: absolute; display: none">
+                    <i style="color: red; font-size: 10px">Descreva o motivo do chamado.</i>
+                </div>
+            </div>
 
             <div id="botao">
                 <input type="submit" value="Salvar" id="submit" class="btn btn-primary btn-sm" />
